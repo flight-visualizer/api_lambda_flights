132bac674beb082b15c7da0e534cec64dd4e71ac
Michael Clark <michaeldavidclark13@gmail.com>
2021-09-04 21:08:30 -0400

    Initial working api with cache check, http call, and cache write with ttl

--- src/api_lambda_flights.py
+++ src/api_lambda_flights.py
@@ -1,80 +1,84 @@
 import os
-import requests
-from typing import List
-from models import InputModel, ResponseModel, FlightsCacheModel
+from typing import Optional
+from models import InputModel, ResponseModel, FlightsCacheModel, ApiResponse
 from services.DynamoService import DynamoService
-# from services.
+from services.HttpClient import HttpClient
 
-
-API_KEY = os.getenv('API_KEY')
-
-flights = DynamoService(
+flights_db = DynamoService(
     table_name = 'cache_dynamo_flights', 
-    model = FlightsCacheModel
+    model = FlightsCacheModel,
+    ttl = 600
 )
 
-# requestService = 
+http_client = HttpClient(
+    endpoint = 'http://api.aviationstack.com/v1/flights',
+    api_key = os.getenv('API_KEY'),
+    responseModel = ResponseModel
+)
 
-def get_realtime_flight_info(parameters: InputModel) -> dict:
+def get_realtime_flight_info(parameters: InputModel) -> Optional[FlightsCacheModel]:
     """
     """
+    response = http_client.query(
+        payload = parameters.dict()
+    )
 
-    try:
-        print(f'Attempting to retrieve flight data with parameters {parameters} ...')
-        response = requests.get(
-            'http://api.aviationstack.com/v1/flights', 
-            params={
-                'airline_iata': parameters.iata,
-                'flight_number': parameters.flight_number,
-                'access_key': API_KEY
-            }
+    if response.data:
+        response = response.data[0]
+        data = FlightsCacheModel(
+            airline_iata = parameters.airline_iata,
+            flight_number = parameters.flight_number,
+            flight_date = response.flight_date,
+            flight_status = response.flight_status,
+            departure = response.departure,
+            arrival = response.arrival
         )
-        print(f'Response received... status: {response.status_code}')
 
-        response = ResponseModel(**response.json())
-
-    except:
-        print('Error querying aviation stack...')
-        raise
-
-    
-    db_records: List[FlightsCacheModel] = []
-    for record in response.data:
-        db_records.append(
-            AirlineCacheModel(
-                iata=record.iata_code,
-                airline=record.airline_name
-            )
-        )
-    return db_records
-
-    #TODO: Marshel response into data model for validation
-
-    response = r.json()
-    print(response["pagination"])
-    print(response["data"][0])
-
-if __name__ == '__main__':
-    handler({'iata': 'AA'}, {})
-
+        print(f'Successfully obtained flight data: {data}')
+        return data
+    else:
+        print(f'Did not find any data with following parameters: {parameters}')
+        return None
 
 # GET /flights/{iata}/{flight_number}
-def lambda_handler(event: dict, context) -> dict:
-    
+def lambda_handler(event: dict, context) -> ApiResponse:
+    """
+    # Query DynamoDB cache to check for the iata, flight #
+    # If present, return data
+    # If NOT present, query aviation stack directly
+    # Update cache with results
+    # Return Results
+    """
     try:
         parameters = InputModel(**event)
         print(f'Input Parameters: {parameters}')
     except:
         raise Exception(f'Incorrect input, event: {event} should be of type {InputModel}')
 
-    get_realtime_flight_info(parameters)
+    cache_result = flights_db.get(parameters.dict())
 
-    
-    # flight_number = event['flight_number'] #'5677'
-    # airline_search_string = event['airline_search_string']
+    if cache_result:
+        return ApiResponse(
+            status_code = 200,
+            message = 'Flight data retrieved from cache',
+            payload = cache_result
+        ).dict()
+        
+    else:
+        data = get_realtime_flight_info(parameters)
+        if data:
+            flights_db.put(data)
+            return ApiResponse(
+                status_code = 200,
+                message = 'Flight data retrieved from HTTP Query, written to cache',
+                payload = data
+            ).dict()
+        else:
+            return ApiResponse(
+                status_code = 400,
+                message = 'Flight data not found in cache or HTTP Query',
+                payload = None
+            ).dict()
 
-    # iata = get_airline_iata(airline_search_string)
-    
-
-    # return response['data']
-    
+# if __name__ == '__main__':
+#     handler({'iata': 'AA'}, {})
--- src/models.py
+++ src/models.py
@@ -1,8 +1,41 @@
 from typing import List, Optional
 from pydantic import BaseModel
 
-class InputModel(BaseModel):
+# -----------------INTERNAL-----------------
+class DepartureModel(BaseModel):
+    airport: str
+    timezone: str
     iata: str
+    icao: str
+    terminal: str
+    gate: str
+    delay: int
+    scheduled: str
+    estimated: str
+    actual: str
+    estimated_runway: str
+    actual_runway: str
+
+# -----------------INTERNAL-----------------
+class ArrivalModel(BaseModel):
+    airport: str
+    timezone: str
+    iata: str
+    icao: str
+    terminal: str
+    gate: str
+    baggage: Optional[str]
+    delay: Optional[int]
+    scheduled: str
+    estimated: str
+    actual: str
+    estimated_runway: str
+    actual_runway: str
+
+# ------------------------------------------
+
+class InputModel(BaseModel):
+    airline_iata: str
     flight_number: int
 
 class ResponseModel(BaseModel):
@@ -13,24 +46,56 @@ class ResponseModel(BaseModel):
         total: int
     pagination: PaginationModel
     
-    class AirlineInfoModel(BaseModel):
-        id: str
-        fleet_average_age: str
-        airline_id: str
-        callsign: str
-        hub_code: Optional[str]
-        iata_code: str
-        icao_code: str
-        country_iso2: str
-        date_founded: Optional[str]
-        iata_prefix_accounting: Optional[str]
-        airline_name: str
-        country_name: str
-        fleet_size: str
-        status: str
-        type: Optional[str]
-    data: List[AirlineInfoModel]
+    class FlightsInfoModel(BaseModel):
+        flight_date: str
+        flight_status: str
 
-class AirlineCacheModel(BaseModel):
-    iata: str
-    airline: str
+        departure: DepartureModel
+        arrival: ArrivalModel
+            
+        class AirlineModel(BaseModel):
+            name: str
+            iata: str
+            icao: str
+        airline: AirlineModel
+        
+        class FlightModel(BaseModel):
+            number: str
+            iata: str
+            icao: str
+        flight: FlightModel
+        
+        class AircraftModel(BaseModel):
+            registration: str
+            iata: str
+            icao: str
+            icao24: str
+        aircraft: Optional[AircraftModel]
+
+        class LiveModel(BaseModel):     
+            updated: str
+            latitude: float
+            longitude: float
+            altitude: float
+            direction: float
+            speed_horizontal: float
+            speed_vertical: float
+            is_ground: bool
+            icao: str
+            icao24: str
+        live: Optional[LiveModel]
+        
+    data: List[FlightsInfoModel]
+
+class FlightsCacheModel(BaseModel):
+    airline_iata: str
+    flight_number: int
+    flight_date: str
+    flight_status: str
+    departure: DepartureModel
+    arrival: ArrivalModel
+
+class ApiResponse(BaseModel):
+    status_code: int
+    message: str
+    payload: Optional[FlightsCacheModel]
